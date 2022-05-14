eeb81f81eb3b5cc2db94da6d9f259d0b93776f01
Jacob Ryerson <jacobryerson@jacobs-mbp.myfiosgateway.com>
2022-05-14 00:08:24 -0400

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout({ children }) {
+    return <>
+        <Header />
+        <main>
+            {children}
+        </main>
+        <Footer />
+
+    </>
+}
